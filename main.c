2e34187c4c3d44a8e8d88053220129160e86c455
rofeldsp <rofeldsp@student.21-school.ru>
2019-10-20 12:17:44 +0300

    4

--- main.c
+++ main.c
@@ -18,20 +18,76 @@ int     ftpow(int number, int power)
 	return (number);
 }
 
-int 	move(node *cell, int tetrnum, int sqrsize)
+int 	linebreak2(node *tetr, int tetrline, int sqrsize)
 {
+	int		cell1;
+	int 	cell2;
+	int		cell3;
+	int 	cell4;
 
+	cell1 = 0;
+	cell2 = 0;
+	cell3 = 0;
+	cell4 = 0;
+	if ((tetr->tetromap[tetrline] & ftpow(2, 19 - sqrsize)) == ftpow(2, 19
+																		- sqrsize))
+		cell1 = 4096;
+	if ((tetr->tetromap[tetrline] & ftpow(2, 18 - sqrsize)) == ftpow(2, 18 -
+																		sqrsize))
+		cell2 = 8192;
+	if ((tetr->tetromap[tetrline] & ftpow(2, 17 - sqrsize)) == ftpow(2, 17 -
+																		sqrsize))
+		cell3 = 16384;
+	if ((tetr->tetromap[tetrline] & ftpow(2, 16 - sqrsize)) == ftpow(2, 16 -
+																		sqrsize))
+		cell4 = 32768;
+	if (tetrline == sqrsize - 1)
+		return (tetr->tetromap[tetrline] != 0 ? -1 : 0);
+	tetr->tetromap[tetrline] >>= tetr->width;
+	if (linebreak2(tetr, tetrline + 1, sqrsize) < 0)
+		return (-1);
+	tetr->tetromap[tetrline + 1] |= (cell1 + cell2 + cell3 + cell4);
+	return (1);
 }
 
-int 	checkmap(int *map, int sqrsize, int tetrnum, node *cell) {
-	if (tetrnum > sqrsize) // проверяем, что текущий ряд входит в карту
-		return (0);
-	if ((map[tetrnum] | cell->tetromap[tetrnum]) == (map[tetrnum] ^ cell->tetromap[tetrnum])) // проверяем, что первый ряд тетроминошки влезает
+int 	linebreak(node *tetr, int sqrsize)
+{
+	int tetrline;
+
+	tetrline = 0;
+	while (tetrline < sqrsize) {
+		if ((tetr->tetromap[tetrline] & ftpow(2, 16 - sqrsize)) == ftpow(2,
+																		 16 - sqrsize))
+			return (linebreak2(tetr, tetrline = 0, sqrsize) == -1 ? -1 : 1);
+		tetrline++;
+	}
+	return (0);
+}
+
+int 	move(node *tetr, int tetrline, int sqrsize)
+{
+	int		ret;
+
+	if ((ret = linebreak(tetr, sqrsize)) != 0)
+		return (ret);
+	while (tetrline < sqrsize)
 	{
-		if (checkmap(map, sqrsize, tetrnum + 1, cell) >= 0) // рекурсивно проверяем след ряд
-			map[tetrnum] = map[tetrnum] | cell->tetromap[tetrnum]; // если все ок, то заполняем карту нашей фигурой
-		else if (move(cell, tetrnum, sqrsize) > 0)
-			if (checkmap(map, sqrsize, tetrnum, cell) >= 0)
+		tetr->tetromap[tetrline] >>= 1;
+		tetrline++;
+	}
+	return (1);
+}
+
+int 	checkmap(int *map, int sqrsize, int tetrline, node *tetr) {
+	if (tetrline == sqrsize) // проверяем, что текущий ряд входит в карту
+		return (0);
+	if ((map[tetrline] | tetr->tetromap[tetrline]) == (map[tetrline] ^ tetr->tetromap[tetrline])) // проверяем, что первый ряд тетроминошки влезает
+	{
+		if (checkmap(map, sqrsize, tetrline + 1, tetr) >= 0) // рекурсивно проверяем след ряд
+			map[tetrline] |= tetr->tetromap[tetrline]; // если все ок, то
+			// заполняем карту нашей фигурой
+		else if (move(tetr, tetrline, sqrsize) > 0)
+			if (checkmap(map, sqrsize, tetrline, tetr) >= 0)
 				return (1);
 		else
 			return (-1); // вот тут надо возвращаться и двигать фигуру, а потом уже если не найдено решение возврашать 0
@@ -41,89 +97,91 @@ int 	checkmap(int *map, int sqrsize, int tetrnum, node *cell) {
 	return (1);
 }
 
-int 	linebreaktwoways(node *cell, int tetrnum, int sqrsize)
+/*
+int 	linebreaktwoways(node *cell, int tetrline, int sqrsize)
 {
-	if (tetrnum + 2 > sqrsize)
+	if (tetrline + 2 > sqrsize)
 		return (0);
-	cell->tetromap[tetrnum] >>= 2;
-	cell->tetromap[tetrnum - 1] >>= 2;
-	cell->tetromap[tetrnum + 1] >>= 2;
-	cell->tetromap[tetrnum + 1] = cell->tetromap[tetrnum + 1] | 49152;
-	cell->tetromap[tetrnum] = cell->tetromap[tetrnum + 1] | 32768;
-	cell->tetromap[tetrnum + 2] = cell->tetromap[tetrnum + 1] | 32768;
+	cell->tetromap[tetrline] >>= 2;
+	cell->tetromap[tetrline - 1] >>= 2;
+	cell->tetromap[tetrline + 1] >>= 2;
+	cell->tetromap[tetrline + 1] = cell->tetromap[tetrline + 1] | 49152;
+	cell->tetromap[tetrline] = cell->tetromap[tetrline + 1] | 32768;
+	cell->tetromap[tetrline + 2] = cell->tetromap[tetrline + 1] | 32768;
 	return (1);
 }
 
-int		linebreakreverse(node *cell, int tetrnum, int sqrsize)
+int		linebreakreverse(node *cell, int tetrline, int sqrsize)
 {
 	int		tetrlength;
 	int 	tetrheight;
 
 	tetrlength = 2;
 	tetrheight = 1;
-	if (cell->tetromap[tetrnum] &= ftpow(2, 16 - sqrsize + 2))
+	if (cell->tetromap[tetrline] &= ftpow(2, 16 - sqrsize + 2))
 		tetrlength = 3;
-	if (cell->tetromap[tetrnum - 2] != 0)
+	if (cell->tetromap[tetrline - 2] != 0)
 		tetrheight = 2;
-	cell->tetromap[tetrnum] >>= tetrlength;
+	cell->tetromap[tetrline] >>= tetrlength;
 	cell->
 
 
 
 }
 
-int     linebreak(node *cell, int sqrsize, int tetrnum, int flag)
+int     linebreak(node *cell, int sqrsize, int tetrline, int flag)
 {
-	if (tetrnum > sqrsize)
+	if (tetrline > sqrsize)
 		return (0);
-	if (cell->tetromap[tetrnum] &= ftpow(2, 16 - sqrsize))
+	if (cell->tetromap[tetrline] &= ftpow(2, 16 - sqrsize)) // не сработает,
+		// тут присвоение идет
 	{
-		if ((cell->tetromap[tetrnum + 1] == 0) && flag == 0 &&
-		cell->tetromap[tetrnum - 1] != 0)
+		if ((cell->tetromap[tetrline + 1] == 0) && flag == 0 &&
+		cell->tetromap[tetrline - 1] != 0)
 			linebreakreverse
-		if ((cell->tetromap[tetrnum + 1] != 0) && flag == 0 &&
-				(cell->tetromap[tetrnum - 1] != 0))
-			if (linebreaktwoways(cell, tetrnum, sqrsize) > 0)
+		if ((cell->tetromap[tetrline + 1] != 0) && flag == 0 &&
+				(cell->tetromap[tetrline - 1] != 0))
+			if (linebreaktwoways(cell, tetrline, sqrsize) > 0)
 				return (1);
 			else
 				return (0);
-		if (tetrnum == sqrsize)
+		if (tetrline == sqrsize)
 			return (-1);
 		else
 		{
-			cell->tetromap[tetrnum] >>= 1; // решить вопрос с переносом
+			cell->tetromap[tetrline] >>= 1; // решить вопрос с переносом
 			// фигуру, где переносить надо не один раз
-			if ((cell->tetromap[tetrnum + 1] | 0) != 0)
-				if (linebreak(cell, sqrsize, tetrnum + 1, flag = 1) > 0)
-					cell->tetromap[tetrnum + 1] = cell->tetromap[tetrnum + 1] |
+			if ((cell->tetromap[tetrline + 1] | 0) != 0)
+				if (linebreak(cell, sqrsize, tetrline + 1, flag = 1) > 0)
+					cell->tetromap[tetrline + 1] = cell->tetromap[tetrline + 1] |
 					32768;
 				else
 					return (-1);
 			else
-				cell->tetromap[tetrnum + 1] = cell->tetromap[tetrnum + 1] |
+				cell->tetromap[tetrline + 1] = cell->tetromap[tetrline + 1] |
 						32768;
 			return (1);
 		}
 	}
 	else if (flag > 0)
-		cell->tetromap[tetrnum] >>= 1;
-	linebreak(cell, sqrsize, tetrnum + 1, flag);
+		cell->tetromap[tetrline] >>= 1;
+	linebreak(cell, sqrsize, tetrline + 1, flag);
 	return (1);
 }
-/*
-int     linebreak(node *cell, int sqrsize, int tetrnum)
+
+int     linebreak(node *cell, int sqrsize, int tetrline)
 {
 	int		ret;
 
-	if (tetrnum > sqrsize) // проверяем, что текущий ряд входит в карту
+	if (tetrline > sqrsize) // проверяем, что текущий ряд входит в карту
 		return (0);
-	if (cell->tetromap[tetrnum] & ftpow(2, 16 - sqrsize) == ftpow(2, 16 - sqrsize))
+	if (cell->tetromap[tetrline] & ftpow(2, 16 - sqrsize) == ftpow(2, 16 - sqrsize))
 	{
-		if ((ret = (linebreak(cell, sqrsize, tetrnum + 1))) > 0)
+		if ((ret = (linebreak(cell, sqrsize, tetrline + 1))) > 0)
 		{
-			cell->tetromap[tetrnum] >> 1;
-			if ((cell->tetromap[tetrnum + 1] | 0) != 0)
-				cell->tetromap[tetrnum + 1] | 32768;
+			cell->tetromap[tetrline] >> 1;
+			if ((cell->tetromap[tetrline + 1] | 0) != 0)
+				cell->tetromap[tetrline + 1] | 32768;
 		}
 		else
 			return (-1);
