b5a68a931743ae7da8472ba88d88f03a3f197c88
rofeldsp <rofeldsp@student.21-school.ru>
2019-10-24 17:39:26 +0300

    17 рабочая новая версия

--- main.c
+++ main.c
@@ -4,11 +4,6 @@
 
 uint64_t     ftpow(uint64_t number, int power, node *tetr)
 {
-//	uint64_t     *cache;
-//	int     i;
-
-//	cache = (uint64_t *)malloc(sizeof(uint64_t) * 64);
-//	cache[2] = number * number;
 	if (power == 2)
 		return (number * number);
 	else if (power == 1)
@@ -16,16 +11,6 @@ uint64_t     ftpow(uint64_t number, int power, node *tetr)
 	else if (power == 0)
 		return (1);
 	return (number = number * tetr->power[power - 1]);
-//	i = 3;
-//	while (i <= power)
-//	{
-//		number = number * tetr->power(i - 1);
-//		cache[i] = cache[i - 1] * number;
-//		i++;
-//	}
-//	number = cache[power];
-//	free (cache);
-//	return (number);
 }
 
 int	ft_sqrt_round(int nb)
@@ -39,74 +24,7 @@ int	ft_sqrt_round(int nb)
 		a++;
 	return (a);
 }
-/*
-int 	clearnumber(int sqrsize, node *tetr)
-{
-	int num;
-	int power;
 
-	power = 14;
-	num = 32768;
-	if (sqrsize == 2)
-		num = 49152;
-	else if (sqrsize == 3)
-		num = 57344;
-	else if (sqrsize == 4)
-		num = 61440;
-	else
-	{
-		while (sqrsize > 3) {
-			num = num * tetr->power[power] * tetr->power[power - 1] *
-				  tetr->power[power - 2] * tetr->power[power - 3];
-			power -= 4;
-			sqrsize -= 4;
-		}
-		if (sqrsize == 1)
-			num = num * tetr->power[power - 1];
-		if (sqrsize == 2)
-			num = num * tetr->power[power - 1] * tetr->power[power - 2];
-		if (sqrsize == 3)
-			num = num * tetr->power[power - 1] * tetr->power[power - 2] *
-					tetr->power[power - 3];
-	}
-	return (num);
-}
-int 	linebreak2(node *tetr, int tetrline, int sqrsize)
-{
-	unsigned short		cell1;
-	unsigned short	 	cell2;
-	unsigned short		cell3;
-	unsigned short	 	cell4;
-
-	if (tetr->tetromap[sqrsize - 1] != 0)
-		return (-1);
-	cell1 = 0;
-	cell2 = 0;
-	cell3 = 0;
-	cell4 = 0;
-	if ((tetr->tetromap[tetrline] & tetr->power[19 - sqrsize]) ==
-	tetr->power[19 - sqrsize])
-		cell1 = tetr->power[19 - sqrsize];
-	if ((tetr->tetromap[tetrline] & tetr->power[18 - sqrsize]) ==
-		tetr->power[18 - sqrsize])
-		cell2 = tetr->power[18 - sqrsize];
-	if ((tetr->tetromap[tetrline] & tetr->power[17 - sqrsize]) ==
-		tetr->power[17 - sqrsize])
-		cell3 = tetr->power[17 - sqrsize];
-	if ((tetr->tetromap[tetrline] & tetr->power[16 - sqrsize]) ==
-		tetr->power[16 - sqrsize])
-		cell4 = tetr->power[16 - sqrsize];
-	if (tetrline == sqrsize - 1)
-		return (tetr->tetromap[tetrline] != 0 ? -1 : 0);
-	tetr->tetromap[tetrline] >>= tetr->width;
-	tetr->tetromap[tetrline] &= clearnumber(sqrsize, tetr);
-	if (linebreak2(tetr, tetrline + 1, sqrsize) < 0)
-		return (-1);
-	tetr->tetromap[tetrline + 1] |= ((cell1 + cell2 + cell3 + cell4) <<
-			(sqrsize - tetr->width));
-	return (1);
-}
-*/
 int 	linebreak(node *tetr, int sqrsize)
 {
 	if (((tetr->tetromap & tetr->power[64 - sqrsize]) != 0) || (
@@ -141,8 +59,6 @@ int		checkmap(uint16_t *map, int sqrsize, node *tetr)
 		if ((*(uint64_t *)(map + 12 - tetr->line) & tetr->tetromap) == 0)
 		{
 			*(uint64_t *)(map + 12 - tetr->line) |= tetr->tetromap; //
-			// будет ли это
-			// значение возвращаться в функцию?
 			return (1);
 		} else if (move(tetr, sqrsize) < 0)
 			return (-1);
@@ -209,9 +125,7 @@ int 	deploy(node *tetr, int tetramount)
 {
 	uint16_t		*map;
 	int 			sqrsize;
-	int 			tetrline;
 
-	tetrline = 0;
 	sqrsize = ft_sqrt_round(4 * tetramount);
 	while (1)
 	{
@@ -309,7 +223,6 @@ node	*addnode(unsigned short *t, node *head, int a)
 	if (tetr == NULL)
 		return (NULL);
 	i = 0;
-//	tetr->power = (unsigned short *)malloc(sizeof(short) * 16);
 	while (i < 64)
 	{
 		tetr->power[i] = ftpow(2, i, tetr);
@@ -341,7 +254,6 @@ node	*createstruct(unsigned short *t, int tetramount)
 	tetr->prev = NULL;
 	tetr->next = NULL;
 	tetr->line = 0;
-//	tetr->power = (unsigned short *)malloc(sizeof(short) * 16);
 	while (i < 64)
 	{
 		tetr->power[i] = ftpow(2, i, tetr);
@@ -375,31 +287,31 @@ int main()
 
 	tetramount = 12;
 	tetro = (unsigned short *)malloc(sizeof(short) * tetramount);
-	tetro[0] = 52224;
-	tetro[1] = 35968;
-	tetro[2] = 27648;
-	tetro[3] = 52224;
-	tetro[4] = 19584;
-	tetro[5] = 57856;
-	tetro[6] = 50240;
-	tetro[7] = 52224;
-	tetro[8] = 27648;
-	tetro[9] = 50240;
-	tetro[10] = 35904;
-	tetro[11] = 58368;
-//	tetro[12] = 58368;
 //	tetro[0] = 52224;
-//	tetro[1] = 57856;
-//	tetro[2] = 35008;
-//	tetro[3] = 50688;
-//	tetro[4] = 19520;
-//	tetro[5] = 35968;
-//	tetro[6] = 50688;
+//	tetro[1] = 35968;
+//	tetro[2] = 27648;
+//	tetro[3] = 52224;
+//	tetro[4] = 19584;
+//	tetro[5] = 57856;
+//	tetro[6] = 50240;
 //	tetro[7] = 52224;
-//	tetro[8] = 57856;
-//	tetro[9] = 61440;
-//	tetro[10] = 57856;
-//	tetro[11] = 50240;
+//	tetro[8] = 27648;
+//	tetro[9] = 50240;
+//	tetro[10] = 35904;
+//	tetro[11] = 58368;
+//	tetro[12] = 58368;
+	tetro[0] = 52224;
+	tetro[1] = 57856;
+	tetro[2] = 35008;
+	tetro[3] = 50688;
+	tetro[4] = 19520;
+	tetro[5] = 35968;
+	tetro[6] = 50688;
+	tetro[7] = 52224;
+	tetro[8] = 57856;
+	tetro[9] = 61440;
+	tetro[10] = 57856;
+	tetro[11] = 50240;
 //	tetro[12] = 50688;
 //	tetro[13] = 61440;
 //	tetro[14] = 35008;
